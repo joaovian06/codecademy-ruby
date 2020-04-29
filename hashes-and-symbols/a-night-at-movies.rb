80240f0ab02390b59003a726d3879d51036deba9
joaovian06 <joaovitor_vian@hotmail.com>
2020-04-29 10:17:07 -0300

    refatorando meus filmes

--- hashes-and-symbols/a-night-at-movies.rb
+++ hashes-and-symbols/a-night-at-movies.rb
@@ -1,58 +1,54 @@
-movies = {
-  movie_1: 3,
-  movie_2: 2,
-  movie_3: 4
+movies = {}
+
+outputs = {
+  init: "Type 'add' to add movies\nType 'update' to update your movies\nType 'display' to show your movies\nType 'delete' to delete your movies\nType 'exit' to quit\n",
+  input_title: "Type a title of movie: ",
+  input_rate: "Type a rating for the movie: ",
+  add_fail: "Your movie already exists!",
+  add_sucess: "Your movie and rating was added sucessfully!",
+  update_fail: "Sorry, nothing to update!",
+  update_sucess: "Your movie and rating was updated sucessfully!",
+  delete_fail: "Nothing to delete!",
+  delete_sucess: "Deleted sucessfully!"
 }
-puts "Type anything: "
-choice = gets.chomp.downcase
 
-# x = true
-# while true do
-# como sair loop com true
+def set_title(outputs)
+  puts outputs[:input_title]
+  title = gets.chomp.to_sym
+end
 
-case choice
+def set_rate(outputs)
+  puts outputs[:input_rate]
+  rate = gets.chomp.to_i
+end
 
-  when "add"
-    puts "Type a title of movie: "
-    title = gets.chomp.to_sym
+def add_movie(outputs, movies, title)
+  return puts output[:movie_exists] if movies.include?(title)
+  movies[title] = set_rate(outputs)
+  puts outputs[:add_sucess]
+end
 
-    puts "Type a rating for the movie: "
-    rating = gets.chomp.to_i
+def update_movie(outputs, movies, title)
+  return puts outputs[:update_fail] if movies[title].nil?
+  movies[title] = set_rate(outputs)
+  puts outputs[:update_sucess]
+end
 
-    if movies[title].nil?
-      movies[title] = rating
-      puts "Your movie and rating was added sucessfully!"
-    else
-      puts "Your movie already exists!"
-    end
+def delete_movie(outputs, movies, title)
+  return puts outputs[:delete_fail] if movies[title].nil?
+  movies.delete(title)
+  puts outputs[:delete_sucess]
+end
 
-  when "update"
-    puts "Type a title of movie: "
-    title = gets.chomp.to_sym
-
-    if movies[title].nil?
-      puts "Sorry, nothing to update!"
-    else
-      puts "Type a rating for the movie: "
-      rating = gets.chomp.to_i
-
-      movies[title] = rating
-      puts "Your movie and rating was added sucessfully!"
-    end
-
-  when "display"
-    movies.each {|movie, rate| puts "#{movie}: #{rate}"}
-
-  when "delete"
-    puts "Type a title of movie: "
-    title = gets.chomp.to_sym
-
-    if movies[title].nil?
-      puts "Nothing to delete!"
-    else movies.delete(title.to_sym)
-      puts "Deleted sucessfully!"
-    end
-
-  else
-    puts "Error!"
+while true do
+  puts outputs[:init]
+  choice = gets.chomp.downcase
+  case choice
+    when "add" then add_movie(outputs, movies, set_title(outputs))
+    when "update" then update_movie(outputs, movies, set_title(outputs))
+    when "display" then movies.each {|movie, rate| puts "#{movie}: #{rate}"}
+    when "delete" then delete_movie(outputs, movies, set_title(outputs))
+    when "exit" then break
+    else puts "Error!"
+  end
 end

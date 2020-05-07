03c7f5fb41ed9b01a6bb20ca9972bc5e86554d5d
joaovian06 <joaovitor_vian@hotmail.com>
2020-05-07 08:50:49 -0300

    metodo de depositos

--- banking-on-ruby/what-youll-be-building.rb
+++ banking-on-ruby/what-youll-be-building.rb
@@ -1,14 +1,15 @@
 class Account
   attr_reader :name, :balance
-  def initialize(name, balance=100)
+  def initialize(name, balance=0)
     @name = name
     @balance = balance
   end
-
   def display_balance(pin_number)
     puts pin_number == pin ? "Balance: $#{@balance}." : pin_error
   end
-
+  def deposite(value, pin_number)
+    pin_number == pin ? @balance += value : pin_error
+  end
   def withdraw(pin_number, amount)
     if pin_number == pin
       @balance -= amount
@@ -17,19 +18,15 @@ class Account
       puts pin_error
     end
   end
-
   private
-
   def pin
     @pin = 1234
   end
-
   def pin_error
-    "Access denied: incorrect PIN."
+    puts "Access denied: incorrect PIN."
   end
 end
-
-my_account = Account.new("Eric", 1_000_000)
+my_account = Account.new("Joao")
 my_account.display_balance(1234)
-my_account.withdraw(1234, 500_000)
+my_account.deposite(500, 12)
 my_account.display_balance(1234)
